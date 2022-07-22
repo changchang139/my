0f3e032ee6220aff0447fb8a54f0a09d1a39dc86
26_chang <genius810613@gmail.com>
2022-07-22 16:52:54 +0800

    chenge buttem to submit,add a lot events in schedule`s js

--- plugins/schedule.js/event.js
+++ plugins/schedule.js/event.js
@@ -2,8 +2,6 @@ $(function () {
   // initialize the external events----------------------------------------------
   function ini_events(ele) {
     ele.each(function () {
-      // create an Event Object (https://fullcalendar.io/docs/event-object)
-      // it doesn't need to have a start or end
       var eventObject = {
         title: $.trim($(this).text()), // use the element's text as the event title
       };
@@ -11,12 +9,12 @@ $(function () {
       // store the Event Object in the DOM element so we can get to it later
       $(this).data("eventObject", eventObject);
 
-      // make the event draggable using jQuery UI
-      $(this).draggable({
-        zIndex: 1070,
-        revert: true, // will cause the event to go back to its
-        revertDuration: 0, //  original position after the drag
-      });
+      // make the event 拖動 using jQuery UI
+      // $(this).draggable({
+      //   zIndex: 1070,
+      //   revert: true, // will cause the event to go back to its
+      //   revertDuration: 0, //  original position after the drag
+      // });
     });
   }
 
@@ -24,12 +22,12 @@ $(function () {
 
   //  initialize the calendar------------------------------------------------------------
   //Date for the calendar events (dummy data)
-  var date = new Date();
+  // var date = new Date();
   // var d = date.getDate(),
   //   m = date.getMonth(),
   //   y = date.getFullYear();
 
-  var Calendar = FullCalendar.Calendar;
+  // var Calendar = FullCalendar.Calendar;
   var Draggable = FullCalendar.Draggable;
 
   var containerEl = document.getElementById("external-events");
@@ -57,7 +55,7 @@ $(function () {
 
   var calendar = new FullCalendar.Calendar(calendarEl, {
     //調月曆長度=============================
-    // height: 530,
+    height: 530,
 
     //轉中文================================
     initalView: "dayGridMonth",
@@ -75,8 +73,8 @@ $(function () {
     themeSystem: "bootstrap",
 
     //選擇日期，跳表單框==================================
-    dateClick: function (data, event, view) {
-      //顯示編輯畫面事件
+
+    dateClick: function (data) {
       // $("#eventFormButtonsure").data("key", data.id);//事件確認按鈕，綁定事件ID
       // $("#eventFormButtonreremove").data("key", data.id);//事件刪除按鈕，綁定事件ID
       $("#dateInput").val(moment(data.date).format("YYYY-MM-DD")); //事件日期，預設填入被點選的日期，不得修改
@@ -85,63 +83,56 @@ $(function () {
       // $("#relaxInput").val(data.eventItem).trigger("change");//事件類型，下拉選單，選單內容為：基本假、特休、事假、其他
       // $("#eventFormModalTitle").text("");//修改modal標題名稱
       // $("#eventFormButtonreremove").show();//顯示刪除按鈕
-      $("#eventFormModal").modal("show"); //顯示編輯視窗，供使用者編輯
+      if (addevenDayForTwo(data)) {
+        //顯示編輯畫面事件
+        $("#eventFormModal").modal("show"); //顯示編輯視窗，供使用者編輯
+      }
     },
   });
 
-  //抓日期======================================
-  var eventa = calendar.getEvents();
-  console.log(eventa);
+  // 一天，畫假上限兩人===============================
 
-  eventa.forEach(function (element) {
-    var evId = element["_def"]["publicId"];
-    var even = calendar.getEventById(evId);
-    console.log(even.title);
-    console.log(even.start.toISOString());
-  });
-  calendar.render();
+  function addevenDayForTwo(data) {
+    // console.log(data);
+    var perDay = data.dayEl.innerText; //捕捉單日裡的內文
+    var perDayCount = perDay.split("\n"); //用斷行切割
+    // console.log(perDayCount.length);
+    if (perDayCount.length == 3) {
+      alert("單日僅能畫休兩位");
+      return false;
+    } else {
+      return true; //回到顯示視窗
+    }
+  }
 
-  //取消按鍵，返回初始值====================================方法一
-  var input = document.querySelector("#eventFormButtonCancel");
-  input.addEventListener("click", function () {
-    document.getElementById("nameInputla").options.selectedIndex = 0;
-    $("#nameInputla").selectpicker("refresh");
-  });
-  input.addEventListener("click", function () {
-    document.getElementById("eventTimeInputla").options.selectedIndex = 0;
-    $("#eventTimeInputla").selectpicker("refresh");
-  });
-  input.addEventListener("click", function () {
-    document.getElementById("relaxInputla").options.selectedIndex = 0;
-    $("#relaxInputla").selectpicker("refresh");
-  });
+  //取消按鍵，返回初始值====================================
+  document
+    .querySelector("#eventFormButtonCancel")
+    .addEventListener("click", function () {
+      document.getElementById("eventTimeInputla").value = 0;
+      document.getElementById("relaxInputla").value = 0;
+    });
 
-  //x按鍵，返回初始值==========================================方法二
+  //x按鍵，返回初始值======================================
   document.querySelector("#inputxxx").addEventListener("click", function (e) {
-    document.querySelector("#nameInputla").value = 0;
     document.querySelector("#eventTimeInputla").value = 0;
     document.querySelector("#relaxInputla").value = 0;
   });
 
   // 確定按鍵，資料呈現==================================
-  var currevents = document
+  document
     .getElementById("eventFormButtonsure")
-    .addEventListener("click", function (e) {
+    .addEventListener("click", function () {
       var dateInput = $("#dateInput");
-      var optionname = $("#nameInputla option:selected");
       var optiontime = $("#eventTimeInputla option:selected");
       var optionrelax = $("#relaxInputla option:selected");
       // alert(optionname.text());
       // console.log(dateInput.val())
       // console.log(optionname.val())
 
-      //選單正確填入，判斷
+      //選單正確填入，判斷===============
       var errorMsg = "";
       var errorflag = false;
-      if (optionname.val() == "0") {
-        errorMsg += "請選擇正確姓名\n";
-        errorflag = true;
-      }
       if (optiontime.val() == "0") {
         errorMsg += "請選擇正確班別\n";
         errorflag = true;
@@ -157,31 +148,32 @@ $(function () {
           .removeAttribute("data-dismiss");
         alert(errorMsg);
       } else {
-        //顏色分假別
+        //顏色分假別=================
         var dayoffcolor;
         var relax = optionrelax.val();
-        var s_reduce = "9";
-
+        var reduce = document.getElementById(relax + "_reduce");
         if (relax == "b") {
           dayoffcolor = "red";
+          reduce.innerHTML--;
+        } else if (relax == "o") {
+          dayoffcolor = "gray";
+        } else if (relax == "t") {
+          dayoffcolor = "blue";
+          reduce.innerHTML++;
         } else if (relax == "s") {
           dayoffcolor = "green";
           //送出特休後，特休-1
-          s_reduce--;
-        } else if (relax == "t") {
-          dayoffcolor = "blue";
-        } else {
-          dayoffcolor = "gray";
+          reduce.innerHTML--;
         }
-        //呈現資料
+        //呈現資料===================
         var myEvent = {
-          title: optionname.text() + "_" + optiontime.text(),
+          title: nameInputla.innerHTML + "_" + optiontime.text(),
           allDay: true,
           start: dateInput.val(),
           color: dayoffcolor,
         };
 
-        //加入事件
+        //加入事件====================
         calendar.addEvent(myEvent);
 
         document
@@ -189,22 +181,47 @@ $(function () {
           //加入收合表單屬性
           .setAttribute("data-dismiss", "modal");
         //事件確定鍵，加入後 選單重置
-        document.querySelector("#nameInputla").value = 0;
         document.querySelector("#eventTimeInputla").value = 0;
         document.querySelector("#relaxInputla").value = 0;
       }
-
-      //刪除鍵
-      //  function aClick() {}
-      //  $("#eventFormButtonreremove").click(function () {
-      //    alert("確認刪除嗎?");
-      //    $("body").remove("click", "#monkey", aClick).find("#monkey").text("");
-      //  });
-
-      //月曆上資料，修改 刪除================================
-
-      // 一天，上限畫假兩人
-
-      // 一月，上限畫八天假
     });
+
+  //特休，判斷==========================================
+  document
+    .getElementById("relaxInputla")
+    .addEventListener("change", function () {
+      var relax = this.value;
+      var reduce = document.getElementById(relax + "_reduce");
+      if (relax == "b") {
+        if (reduce.innerHTML == 0) {
+          alert("基本假已使用完，請選擇其他假別");
+          document.querySelector("#relaxInputla").value = 0;
+        }
+      } else if (relax == "s") {
+        if (reduce.innerHTML == 0) {
+          alert("特休已使用完，請選擇其他假別");
+          document.querySelector("#relaxInputla").value = 0;
+        }
+      } else if (relax == "t") {
+        if (reduce.innerHTML >= 14) {
+          alert("當年度特休已使用完，請選擇其他假別");
+          document.querySelector("#relaxInputla").value = 0;
+        }
+      }
+    });
+  //月曆上資料，修改 刪除================================
+
+  // 一月，上限畫八天假
+
+  //(最後送出)抓所有資料 送出======================================
+  var eventa = calendar.getEvents();
+  console.log(eventa);
+
+  eventa.forEach(function (element) {
+    var evId = element["_def"]["publicId"];
+    var even = calendar.getEventById(evId);
+    console.log(even.title);
+    console.log(even.start.toISOString());
+  });
+  calendar.render();
 });
